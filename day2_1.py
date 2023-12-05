fb56891520de7583db02fa3c91bd5033e0c1f825
Peter Aandahl <peter.aandahl@axis.com>
2023-12-05 17:07:24 +0100

    Day 2 in progress

--- day2_1.py
+++ day2_1.py
@@ -6,8 +6,13 @@ with open('sample2_1.txt', newline='') as csvfile:
     total = 0
     for row in reader:
         data = ' '.join([str(elem) for elem in row])
-        id = data.split(':')[0].split(' ')[1]
+        game_id = data.split(':')[0].split(' ')[1]
         set_data = data.split(':')[1]
         no_sets = set_data.count(';')
 
-        print(set_data, no_sets)
+        for i in range(0, no_sets):
+            set = set_data.split(';')[i][1:].replace('  ', ' ').split(' ')
+            print(game_id, i, set)
+
+
+        print("Next row")
