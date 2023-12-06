411d7e4a60214ec04ee0ffebb3d4a4e20ab1dbfa
Peter Aandahl <peter.aandahl@axis.com>
2023-12-06 21:06:01 +0100

    Day 2 in progress

--- day2_1.py
+++ day2_1.py
@@ -4,6 +4,8 @@ import csv
 with open('sample2_1.txt', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     total = 0
+    max_result = {'red': 12, 'green': 13, 'blue': 14}
+
     for row in reader:
         data = ' '.join([str(elem) for elem in row])
         game_id = data.split(':')[0].split(' ')[1]
@@ -12,7 +14,10 @@ with open('sample2_1.txt', newline='') as csvfile:
 
         for i in range(0, no_sets):
             set = set_data.split(';')[i][1:].replace('  ', ' ').split(' ')
-            print(game_id, i, set)
+            set_dict = dict(map(lambda x: (set[x + 1], int(set[x])), range(len(set) - 1)[::2]))
+            print(game_id, i,set_dict)
+            if 'red' in set_dict.keys():
+                print(max_result['red'])
 
 
         print("Next row")
